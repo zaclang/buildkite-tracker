e646227644425667e4ce79c78aeeb6b216a1c521
Zac Lang <zac.wr.lang@gmail.com>
2018-05-07 19:21:31 +1000

    add link to build

--- renderer/components/BubbleChart/index.jsx
+++ renderer/components/BubbleChart/index.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactBubbleChart from '../../vendor/react-bubble-chart/lib/ReactBubbleChart';
 import get from 'lodash.get';
+import { shell } from 'electron';
 
 const colorLegend = [
-  // reds from dark to light
   { color: '#B0DF21', textColor: '#000', text: 'passing' },
+  { color: '#ffdc00', textColor: '#000', text: 'blocked' },
   { color: '#F83E23', textColor: '#fff', text: 'failing' }
 ];
 
 const states = {
   FAILED: 100,
-  PASSED: 1,
+  PASSED: 10,
   BLOCKED: 25
 };
 
@@ -19,27 +20,24 @@ export default ({ data }) => {
     const builds = get(item, 'node.builds.edges');
     const lastBuild = get(builds, '0.node');
 
-    let val = 0;
+    let score = 0;
     builds.forEach(({ node }) => {
-      val += states[node.state]
+      score += states[node.state]
     });
 
     return {
-      _id: `${get(item, 'node.name')} - ${get(lastBuild, 'createdBy.name')}`,
-      value: val,
-      colorValue: val,
-      selected: false
+      // _id: `${get(item, 'node.name')} - ${get(lastBuild, 'createdBy.name')}`,
+      _id: `${get(item, 'node.name')} - ${score}`,
+      value: score,
+      colorValue: score,
+      url: lastBuild.url
   }});
-
-  console.log(bubbles)
 
   return (
     <ReactBubbleChart
       colorLegend={colorLegend}
-      selectedColor="#737373"
-      selectedTextColor="#d9d9d9"
-      // onClick={Actions.loadArticlesForEntity.bind(Actions)}
+      onClick={({ url }) => shell.openExternal(url)}
       data={bubbles}
-      />
+    />
   )
 }
